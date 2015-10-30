e1a63e1f46295c2e7b9589a2288884c12d42c0b1
Live System User <liveuser@unknown485d60e7244f.localdomain>
2015-10-30 15:53:36 +0100

    Homework1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,30 @@
+// Fibonacci-Folge
 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cout <<"Type in an integer N (N<47) and press return:"<< endl;
+    cin >> N;
+    
+    int f0=0, f1=1, ftemp;
+    if(N==0)
+        cout <<"Result:"<<f0<< endl;
+    else 
+            if(N==1)
+            cout <<"Result:"<<f1<< endl;
+        else{
+            for(int i=0; i<N-1; i++){
+                ftemp=f1;
+                f1=f1+f0;
+                f0=ftemp;
+                }
+    
+    cout <<"Result:"<< f1 << endl;
+        }
+    
+    return 0;
+    
+}
